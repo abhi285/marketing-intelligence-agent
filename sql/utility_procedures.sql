f76fdcee56dbdd73b4e4f6d5665b13669053a21a
Abhijith Antony <43300913+abhi285@users.noreply.github.com>
2025-11-24 07:51:11 -0500

    Create utility procedures for file URLs and email
    Added procedures for generating presigned URLs and sending emails.

---
+++ sql/utility_procedures.sql
@@ -0,0 +1,54 @@
+use role SF_Intelligence_Demo;
+
+CREATE OR REPLACE PROCEDURE Get_File_Presigned_URL_SP(
+    RELATIVE_FILE_PATH STRING, 
+    EXPIRATION_MINS INTEGER DEFAULT 60
+)
+RETURNS STRING
+LANGUAGE SQL
+COMMENT = 'Generates a presigned URL for a file in the static @INTERNAL_DATA_STAGE. Input is the relative file path.'
+EXECUTE AS CALLER
+AS
+$$
+DECLARE
+    presigned_url STRING;
+    sql_stmt STRING;
+    expiration_seconds INTEGER;
+    stage_name STRING DEFAULT '@SF_AI_DEMO.DEMO_SCHEMA.INTERNAL_DATA_STAGE';
+BEGIN
+    expiration_seconds := EXPIRATION_MINS * 60;
+
+    sql_stmt := 'SELECT GET_PRESIGNED_URL(' || stage_name || ', ' || '''' || RELATIVE_FILE_PATH || '''' || ', ' || expiration_seconds || ') AS url';
+    
+    EXECUTE IMMEDIATE :sql_stmt;
+    
+    
+    SELECT "URL"
+    INTO :presigned_url
+    FROM TABLE(RESULT_SCAN(LAST_QUERY_ID()));
+    
+    RETURN :presigned_url;
+END;
+$$;
+
+-- Create stored procedure to send emails to verified recipients in Snowflake
+
+CREATE OR REPLACE PROCEDURE send_mail(recipient TEXT, subject TEXT, text TEXT)
+RETURNS TEXT
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.11'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'send_mail'
+AS
+$$
+def send_mail(session, recipient, subject, text):
+    session.call(
+        'SYSTEM$SEND_EMAIL',
+        'ai_email_int',
+        recipient,
+        subject,
+        text,
+        'text/html'
+    )
+    return f'Email was sent to {recipient} with subject: "{subject}".'
+$$;
