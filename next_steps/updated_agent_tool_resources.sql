38289ba8165e79e7e07b64b22b87289975271d17
Abhijith Antony <43300913+abhi285@users.noreply.github.com>
2025-12-03 06:24:38 -0500

    Add SQL file for Forecast_ROAS procedure

---
+++ next_steps/updated_agent_tool_resources.sql
@@ -0,0 +1,10 @@
+"Forecast_ROAS": {
+  "execution_environment": {
+    "type": "warehouse",
+    "warehouse": "SNOW_INTELLIGENCE_DEMO_WH",
+    "query_timeout": 0
+  },
+  "identifier": "SF_AI_DEMO.DEMO_SCHEMA.FORECAST_ROAS_SP",
+  "name": "FORECAST_ROAS_SP(VARCHAR, NUMBER)",
+  "type": "procedure"
+}
